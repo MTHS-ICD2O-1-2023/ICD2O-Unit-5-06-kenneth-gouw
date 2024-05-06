cd61ef02dcfd0747a56737262de4f705ea5ac6be
kenneth-gouw <159185824+kenneth-gouw@users.noreply.github.com>
2024-05-06 18:39:51 +0000

    gg

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Kenneth All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Kenneth
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  const firstNumber = parseInt(document.getElementById("first-number").value)
+  const secondNumber = parseInt(document.getElementById("second-number").value)
+  let counter = 0
+  let answer = 0
+
+  while (counter < Math.abs(secondNumber)) {
+    answer += Math.abs(firstNumber)
+    counter++
+  }
+
+  if (((firstNumber < 0) && (secondNumber > 0)) || ((firstNumber > 0) && (secondNumber < 0))) {
+    answer = answer * -1
+  }
+
+  document.getElementById("answer").innerHTML = firstNumber + " x " + secondNumber + " = " + answer
+} 
